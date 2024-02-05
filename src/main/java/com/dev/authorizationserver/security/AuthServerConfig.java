2343d55bcaf8c908979242ecde69fac91a1a25bb
Marcos Vinicius <contato.marcoscandido@gmail.com>
2024-02-05 16:16:54 -0300

    forma mais enxuta de criar um ClientDetailService

--- src/main/java/com/dev/authorizationserver/security/AuthServerConfig.java
+++ src/main/java/com/dev/authorizationserver/security/AuthServerConfig.java
@@ -1,5 +1,8 @@
 package com.dev.authorizationserver.security;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,31 +15,71 @@ import org.springframework.security.oauth2.provider.client.InMemoryClientDetails
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthServerConfig  extends AuthorizationServerConfigurerAdapter {
-	
-	
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
 	/**
 	 * 
-	 * 
+	 * Sobrescreve o método configure() para configurar a instância do
+	 * ClientDetailsService
 	 * 
 	 */
 	@Override
-	public void configure(ClientDetailsServiceConfigurer clients ) throws Exception {
-		
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+		/*
+		 * 
+		 * Cria uma instância utilizando a implementação do ClientDetailsService.
+		 * 
+		 */
+
 		var service = new InMemoryClientDetailsService();
-		
+
+		/*
+		 * Cria uma instância de ClientDetails e configura os detalhes necessários sobre
+		 * o cliente
+		 */
+
 		var cd = new BaseClientDetails();
-		
-		
+		cd.setClientId("client");
+		cd.setClientSecret("secret");
+		cd.setScope(List.of("read"));
+		cd.setAuthorizedGrantTypes(List.of("password"));
+
+		/**
+		 * Adiciona a instância de ClientDetails ao InMemoryClientDetailsService.
+		 */
+
+		service.setClientDetailsStore(Map.of("client", cd));
+
+		/**
+		 * Configura o ClientDetailsService para ser usado pelo nosso servidor de
+		 * autorização.
+		 */
+		clients.withClientDetails(service);
+
 	}
+
 	
-/*
- * 
- * Agora podemos alterar a classe AuthServerConfig para registrar o AuthenticationManager
- 
- * 
- */
+	/*
+	 * 
+	 * Forma mais enxuta de configurar 
+	 * 
+	 * @Override
+	   public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("client").secret("secret").authorizedGrantTypes("password").scopes("read");
+	    }
+	 * 
+	 */
 	
+
+	/*
+	 * 
+	 * Agora podemos alterar a classe AuthServerConfig para registrar o
+	 * AuthenticationManager
+	 * 
+	 * 
+	 */
+
 	/*
 	 * 
 	 * Injeta o gerenciador de autenticação instância do contexto
@@ -45,19 +88,14 @@ public class AuthServerConfig  extends AuthorizationServerConfigurerAdapter {
 	 */
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
-	
+
 	/*
 	 * Sobrescreve o método configure() para configurar o AuthenticationManager
 	 */
 	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints ) {
-		
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+
 		endpoints.authenticationManager(authenticationManager);
 	}
-	
-	
-	
-	
-	
+
 }
